eea89e2b0264f17afadeb3fbb7e6aa6073b8b4c8
Sven Welte <sven.welte@mindmatters.de>
2017-04-02 15:36:25 +0200

    initial commit

---
+++ src/test/java/de/svenwelte/KataApplicationTests.java
@@ -0,0 +1,16 @@
+package de.svenwelte;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
